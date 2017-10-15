d50ab9fd4b36640feb3d61311634b4c43277206a
vdrg <vrdragicevic@gmail.com>
2017-10-15 20:45:33 -0300

    Add comments to examples

--- example/get.py
+++ example/get.py
@@ -1,19 +1,10 @@
 from kademlia_iic2523.ipc import ipcSend
 import sys
 
+# Callback que se ejecutará con la respuesta del nodo
 def onResponse(message):
     print(message)
 
+# Envía el comando "get key" al nodo
 ipcSend("get " + sys.argv[1], onResponse)
-#  if os.path.exists(".ipc"):
-    #  client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    #  client.connect(".ipc")
 
-    #  client.send(("get " + sys.argv[1]).encode("utf-8"))
-
-    #  datagram = client.recv(1024)
-    #  if datagram:
-        #  print(datagram.decode("utf-8"))
-    #  client.close()
-#  else:
-    #  print("Couldn't Connect!")
--- example/node.py
+++ example/node.py
@@ -6,22 +6,29 @@ import asyncio
 
 from kademlia_iic2523.server import Server
 
-PORT = sys.argv[1] # Este puerto será único para cada alumno
-initialNodes = [("127.0.0.1", 8468)] # IP/PUERTO del servidor inicial
+# Este puerto será único para cada alumno
+PORT = sys.argv[1]
+
+# IP/PUERTO del servidor inicial que estará corriendo en el clúster
+initialNodes = [("127.0.0.1", 8468)] 
     
 # Pueden descomentar la línea siguiente para obtener más información sobre las operaciones
 #logging.basicConfig(level=logging.DEBUG)
 
-# PARTE IMPORTANTE
-async def runCommand(node, command):
-    commands = command.split(" ")
-    if (commands[0] == "get"):
-        print("BEFORE")
-        result = await node.get(commands[1])
+# PARTE IMPORTANTE:
+# Procesa los comandos recibidos por get.py y set.py
+# Opcionalmente, retorna un string, que es enviada como respuesta
+async def runCommand(node, message):
+    command = message.split(" ")
+    if (command[0] == "get"):
+        # Consulta a la DHT por la llave recibida
+        result = await node.get(command[1])
+
+        # Si se encontraron valores, enviarlos de vuelta
         return result if result else "Not found."
-    elif (commands[0] == "set"):
-        # Guardará el valor en "command[2]" para la llave en "command[1]"
-        success = await node.set(commands[1], commands[2])
+    elif (command[0] == "set"):
+        # Guardará el valor en "command[2]" con la llave en "command[1]"
+        success = await node.set(command[1], command[2])
         if (success):
             print("Value set.")
         else:
@@ -33,12 +40,14 @@ loop = asyncio.get_event_loop()
 server = Server()
 server.listen(PORT, runCommand)
 print("Listening at port", PORT) 
+
+# Conectarse al nodo inicial
 loop.run_until_complete(server.bootstrap(initialNodes))
 
 try:
     loop.run_forever()
-except Exception as e:
-    print(e)
+except KeyboardInterrupt as e:
+    pass
 finally:
     print("Closing node")
     server.stop()
